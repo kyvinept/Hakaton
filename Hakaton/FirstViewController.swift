e1cef5a4736e5819a44fc76add42aae63626db6a
Sergey Pogrebnyak <sergey.pogrebnuak@nixsolutions.com>
2018-11-17 13:26:31 +0200

    add alert

--- Hakaton/FirstViewController.swift
+++ Hakaton/FirstViewController.swift
@@ -30,4 +30,32 @@ class FirstViewController: UIViewController {
          print("0")
     }
 
+    @IBAction func iphoneAction(_ sender: Any) {
+        let index = Int.random(in: 0..<array.count)
+        print(array[index])
+
+        let alert = UIAlertController(title: "Alert", message: array[index], preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Click", style: .default, handler: nil))
+
+        var imageView = UIImageView(frame: CGRect(x: 220, y: 10, width: 40, height: 40))
+        imageView.image = UIImage(named: "nixLogo")
+
+        alert.view.addSubview(imageView)
+
+
+        self.present(alert, animated: true, completion: nil)
+    }
+    let array = ["Ты совершенство во всём.",
+                 "Лучше тебя не бывает.",
+                 "Будь на волне успеха.",
+                 "Действуй. Решай. Побеждай.",
+                 "Там где ты − всегда праздник!",
+                 "Возьми СЧАСТЬЕ за правило!",
+                 "Верь в мечты! Они сбываются.",
+                 "Успех – для лучших... Таких, как ты!",
+                 "Мечтай. Верь в себя. Побеждай!",
+                 "Ярких достижений и успехов во всём!",
+                 "Успехов",
+                 "Добра",
+                 "Удачи"]
 }
