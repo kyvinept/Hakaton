c09f244143907ce76bcadca2873776ae448e0c46
Silchenko <kyvinept@gmail.com>
2018-11-17 14:11:14 +0200

    fix stars

--- Hakaton/FirstViewController.swift
+++ Hakaton/FirstViewController.swift
@@ -13,11 +13,28 @@ class FirstViewController: UIViewController {
 
     @IBOutlet weak var firstButton: OBShapedButton!
     @IBOutlet weak var zeroButton: OBShapedButton!
-
+    let stars = [UIImage(named: "star"), UIImage(named: "star-1"), UIImage(named: "star-2"), UIImage(named: "star-3"), UIImage(named: "star-4")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        createStars()
+    }
+    
+    private func createStars() {
+        let countStars = 50
+        for i in 0...countStars {
+            let imageView = UIImageView(image: stars[Int.random(in: 0...stars.count-1)])
+            self.view.addSubview(imageView)
+            imageView.center = self.view.center
+            
+            let anim = CABasicAnimation(keyPath: "position")
+            anim.fromValue = imageView.center
+            anim.toValue = CGPoint(x: 1000.0 * sin(Double(i)*360/Double(countStars)), y: 1000.0 * cos(Double(i)*360.0/Double(countStars)))
+            anim.beginTime = Double.random(in: 0...3)
+            anim.duration = 3
+            anim.repeatCount = .infinity
+            imageView.layer.add(anim, forKey: "position")
+        }
     }
     
 
--- Hakaton/ViewController.swift
+++ Hakaton/ViewController.swift
@@ -13,35 +13,15 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         self.navigationController?.navigationBar.isHidden = true
-        // Do any additional setup after loading the view, typically from a nib.
-        createStars()
-    }
-    let stars = [UIImage(named: "star"), UIImage(named: "star-1"), UIImage(named: "star-2"), UIImage(named: "star-3"), UIImage(named: "star-4")]
-    @IBAction func buttonAction(_ sender: Any) {
-
+        
         let VControl = UIStoryboard.init(name: "First",
-                                          bundle: nil ).instantiateViewController(
+                                         bundle: nil ).instantiateViewController(
                                             withIdentifier: "FirstViewController")
-
-
         self.navigationController?.pushViewController(VControl, animated: false)
     }
-    private func createStars() {
-        let countStars = 50
-        for i in 0...countStars {
-            let imageView = UIImageView(image: stars[Int.random(in: 0...stars.count-1)])
-            self.view.addSubview(imageView)
-            imageView.center = self.view.center
+    @IBAction func buttonAction(_ sender: Any) {
 
-            let anim = CABasicAnimation(keyPath: "position")
-            anim.fromValue = imageView.center
-            anim.toValue = CGPoint(x: 1000.0 * sin(Double(i)*360/Double(countStars)), y: 1000.0 * cos(Double(i)*360.0/Double(countStars)))
-            anim.beginTime = Double.random(in: 0...3)
-            anim.duration = 3
-            anim.repeatCount = .infinity
-            imageView.layer.add(anim, forKey: "position")
-        }
+        
     }
-
 }
 
