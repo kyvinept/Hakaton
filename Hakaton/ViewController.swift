2c7ef539a909e1b3325f20a85e464fd819956997
Silchenko <kyvinept@gmail.com>
2018-11-17 10:33:17 +0200

    init

---
+++ Hakaton/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Hakaton
+//
+//  Created by Silchenko on 17.11.2018.
+//  Copyright © 2018 Silchenko. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
